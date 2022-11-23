48ad35519cb9891ca31c92561f523b6cf3db643c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-23 23:06:28 +0600

    login page solved

---
+++ src/components/Spinner/SmallSpinner.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SmallSpinner() {
+  return (
+    <div className="w-8 h-8  border-4 border-dashed rounded-full animate-spin dark:border-green-400"></div>
+  );
+}
+
+export default SmallSpinner;
