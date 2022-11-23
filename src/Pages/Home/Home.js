3862fa5a83f7acbdf21c2632850c5624e0c8e755
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-23 22:07:19 +0600

    products categoris add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ProductCategoris from "./ProductCategoris";
 import TopSection from "./TopSection";
 
 const Home = () => {
   return (
     <div>
       <TopSection />
+      <ProductCategoris />
     </div>
   );
 };
---
+++ src/Pages/Home/ProductCategoris.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProductCategoris = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <h1 className="text-center text-black text-3xl p-5 font-bold">
+        ProductCategoris
+      </h1>
+      <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+        <Link
+          to="/"
+          className="px-8 py-3 text-lg font-semibold rounded dark:bg-green-400 dark:text-gray-900"
+        >
+          {" "}
+          Suspendisse
+        </Link>
+        <Link
+          to="/"
+          className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100"
+        >
+          Malesuada
+        </Link>
+        <Link
+          to="/"
+          className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100"
+        >
+          Malesuada
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCategoris;
