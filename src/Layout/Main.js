d1f0510de07f6c70514ea8a4e7ef3258abbf7221
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-23 21:18:49 +0600

    Primary Button add

---
+++ src/Layout/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../Pages/Shared/Footer";
+import Navbar from "../Pages/Shared/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
