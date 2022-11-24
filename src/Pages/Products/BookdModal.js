d32b2174879e6558168bed3a22e3520dd8e871b9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-24 14:26:13 +0600

    name dynamic add

--- src/Pages/Products/BookdModal.js
+++ src/Pages/Products/BookdModal.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthProvider";
+import PrimaryButton from "../../PrimaryButton";
 
 function BookdModal({ item, setItem }) {
   const { user } = useContext(AuthContext);
@@ -9,14 +10,14 @@ function BookdModal({ item, setItem }) {
     <div>
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
       <div className="modal">
-        <div className="modal-box relative">
+        <div className="modal-box relative text-black bg-white">
           <label
             htmlFor="booking-modal"
             className="btn btn-sm btn-circle absolute right-2 top-2"
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold ">{item.name}</h3>
+          <h3 className="text-lg font-bold ">{item?.name}</h3>
           <form className="space-y-6 ng-untouched ng-pristine ng-valid">
             <div className="space-y-3 text-sm ">
               <input
@@ -54,12 +55,12 @@ function BookdModal({ item, setItem }) {
                 className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-100 dark:text-gray-900 focus:dark:border-green-400"
               />
             </div>
-            <button
+            <PrimaryButton
               type="submit"
-              className="block w-full p-3 text-center rounded-sm text-white bg-[#3A4256]"
+              classes="block w-full p-3 text-center rounded-sm text-white"
             >
               Submit
-            </button>
+            </PrimaryButton>
           </form>
         </div>
         <div></div>
