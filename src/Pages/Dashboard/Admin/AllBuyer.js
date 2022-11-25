af2485368a91f2c5b8eee94ba699f12a5f38bc94
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-25 17:50:33 +0600

    all buyer and all seller data load in dynamic

--- src/Pages/Dashboard/Admin/AllBuyer.js
+++ src/Pages/Dashboard/Admin/AllBuyer.js
@@ -21,6 +21,9 @@ const AllBuyer = () => {
   console.log(buyers);
   return (
     <div className="container mx-auto px-4 sm:px-8">
+      <div className="text-center">
+        <h1>All Buyers Information</h1>
+      </div>
       <div className="py-8">
         <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
           <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
