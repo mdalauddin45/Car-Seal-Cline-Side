d5cd69c622778f0c3ed5dd4d533d5a647fb6196a
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-25 20:15:45 +0600

    seller menu icon add

--- src/Pages/Dashboard/Sellers/SellerMenu.js
+++ src/Pages/Dashboard/Sellers/SellerMenu.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { FingerPrintIcon, UserPlusIcon } from "@heroicons/react/24/solid";
+import { CloudArrowUpIcon, PencilSquareIcon } from "@heroicons/react/24/solid";
 
 const SellerMenu = () => {
   return (
     <>
       <NavLink
-        to="manage-homes"
+        to="manage-products"
         className={({ isActive }) =>
           `flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${
             isActive ? "bg-gray-300  text-gray-700" : "text-gray-600"
           }`
         }
       >
-        <FingerPrintIcon className="w-5 h-5" />
+        <PencilSquareIcon className="w-5 h-5" />
 
-        <span className="mx-4 font-medium">Manage Homes</span>
+        <span className="mx-4 font-medium">Manage Products</span>
       </NavLink>
 
       <NavLink
@@ -26,9 +26,9 @@ const SellerMenu = () => {
           }`
         }
       >
-        <UserPlusIcon className="w-5 h-5" />
+        <CloudArrowUpIcon className="w-5 h-5" />
 
-        <span className="mx-4 font-medium">Add Home</span>
+        <span className="mx-4 font-medium">Add A Product</span>
       </NavLink>
     </>
   );
