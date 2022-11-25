f5dd1727be08a7508a3ad9dff98fc5f3f4f805d8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-25 10:23:57 +0600

    home category dynamicaly load

--- src/Pages/Products/HomeCategory.js
+++ src/Pages/Products/HomeCategory.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import HCategory from "./HCategory";
-import HomeProducts from "./HomeTemporary/HomeProducts";
-
+import { Link } from "react-router-dom";
 const HomeCategory = () => {
   const [category, setCategory] = useState([]);
+  const [uniqData, setUniqData] = useState();
 
   useEffect(() => {
     fetch("http://localhost:5000/products")
@@ -13,13 +12,29 @@ const HomeCategory = () => {
         // console.log(data);
         setCategory(data);
       });
-  }, []);
-  console.log(category);
+    const getUniqeData = (data, property) => {
+      let newVal = data?.map((curElem) => {
+        return curElem[property];
+      });
+      newVal = [...new Set(newVal)];
+      setUniqData(newVal);
+      // console.log(newVal);
+    };
+    const categoryOnlyData = getUniqeData(category, "category");
+    // console.log(uniqData);
+  }, [category]);
+
   return (
     <div>
-      <div>
-        {category.map((cat) => (
-          <HCategory key={cat._id} cat={cat}></HCategory>
+      <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start text-black">
+        {uniqData?.map((cat, i) => (
+          <Link
+            key={i}
+            to={`/category/${cat}`}
+            className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100"
+          >
+            {cat}
+          </Link>
         ))}
       </div>
     </div>
