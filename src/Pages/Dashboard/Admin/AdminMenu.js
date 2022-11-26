972ae108a1185dc640149e937231d7c176d42b39
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-26 10:32:08 +0600

    report irem page add

--- src/Pages/Dashboard/Admin/AdminMenu.js
+++ src/Pages/Dashboard/Admin/AdminMenu.js
@@ -1,4 +1,8 @@
-import { UserGroupIcon, UserIcon } from "@heroicons/react/24/solid";
+import {
+  ShieldCheckIcon,
+  UserGroupIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
@@ -17,7 +21,6 @@ const AdminMenu = ({ role }) => {
 
         <span className="mx-4 font-medium">All Buyers</span>
       </NavLink>
-
       <NavLink
         to="all-sellers"
         className={({ isActive }) =>
@@ -29,6 +32,18 @@ const AdminMenu = ({ role }) => {
         <UserIcon className="w-5 h-5" />
 
         <span className="mx-4 font-medium"> All Sellers</span>
+      </NavLink>
+      <NavLink
+        to="all-sellers"
+        className={({ isActive }) =>
+          `flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${
+            isActive ? "bg-gray-300  text-gray-700" : "text-gray-600"
+          }`
+        }
+      >
+        <ShieldCheckIcon className="w-5 h-5" />
+
+        <span className="mx-4 font-medium"> Reported Item</span>
       </NavLink>
     </>
   );
